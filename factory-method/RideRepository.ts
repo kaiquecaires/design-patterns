0f14020ef08307247363521b3e480f695f689fe2
kaiquecaires <caires.kaique@gmail.com>
2025-04-14 08:24:48 -0300

    feat: added ride repository

---
+++ factory-method/RideRepository.ts
@@ -0,0 +1,30 @@
+import Ride from "./Ride";
+
+export default interface RideRepository {
+    save(ride: Ride): Promise<void>;
+    update(ride: Ride): Promise<void>;
+    getById(rideId: string): Promise<Ride>;
+}
+
+export class RideRepositoryMemory implements RideRepository {
+    rides: Ride[];
+
+    constructor() {
+        this.rides = [];
+    }
+
+    async save(ride: Ride): Promise<void> {
+        this.rides.push(ride);
+    }
+
+    async getById(rideId: string): Promise<Ride> {
+        const ride = this.rides.find(ride => ride.rideId === rideId);
+        if (!ride) throw new Error("Ride not found");
+        return ride;
+    }
+
+    async update(ride: Ride): Promise<void> {
+        const index = this.rides.findIndex(existingRide => existingRide.rideId === ride.rideId);
+        this.rides[index] = ride;
+    }
+}
